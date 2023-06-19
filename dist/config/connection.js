c02a17d01aeef2cf57b9593559703cfe4deaeec4
beauspot <beauspot@outlook.com>
2023-06-19 13:15:52 +0100

    compiled.

---
+++ dist/config/connection.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_typescript_1 = require("sequelize-typescript");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const connection = new sequelize_typescript_1.Sequelize({
+    dialect: "postgres",
+    host: "localhost",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: false,
+    models: ["TasksModel", "AuthModel"],
+});
+exports.default = connection;
