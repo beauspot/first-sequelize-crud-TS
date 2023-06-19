5ca494354690a4c66946596c2850fab187969f2b
beauspot <beauspot@outlook.com>
2023-06-19 13:16:13 +0100

    compiled.

--- dist/server.js
+++ dist/server.js
@@ -4,24 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
+const hpp_1 = __importDefault(require("hpp"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const morgan_1 = __importDefault(require("morgan"));
-const xss_clean_1 = __importDefault(require("xss-clean"));
 const helmet_1 = __importDefault(require("helmet"));
 const express_session_1 = __importDefault(require("express-session"));
 const compression_1 = __importDefault(require("compression"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_1 = __importDefault(require("express"));
 dotenv_1.default.config();
+const connection_1 = __importDefault(require("./config/connection"));
 const app = (0, express_1.default)();
+app.disable("x-powered-by");
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, morgan_1.default)("tiny"));
 app.use((0, compression_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use((0, helmet_1.default)());
-app.use((0, xss_clean_1.default)());
+app.use((0, hpp_1.default)());
 app.use((0, cors_1.default)({
     credentials: true,
 }));
@@ -43,7 +45,9 @@ app.get("/", (req, res, next) => {
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3030;
 const start = async () => {
     try {
-        app.listen(SERVER_PORT, () => console.log(`Server started on port ${SERVER_PORT}`));
+        await connection_1.default.authenticate();
+        console.log("Connected to the Database.");
+        app.listen(SERVER_PORT, () => console.log(`Server started on port ${SERVER_PORT}!!!`));
     }
     catch (error) {
         console.error(error);
